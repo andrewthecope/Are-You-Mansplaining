e59fc2de069b0f17c0edfb11a694dd2f3f72bf65
Andrew Cope <andrew@thecope.net>
2017-11-13 16:44:38 -0800

    Demo

--- Are You Mansplaining/ViewController.swift
+++ Are You Mansplaining/ViewController.swift
@@ -13,16 +13,52 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        resultLabel.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func areYouPressed(_ sender: Any) {
+        
+        isThisMansplaining(text: textView.text)
+        
     }
+    
+    func isThisMansplaining(text: String) {
+        
+        let encodedText = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        
+        let url = "https://hackdavis-iosworkshop.herokuapp.com/mansplain?text=%20\(encodedText!)%20"
+        
+        Alamofire.request(url).responseJSON { response in
+            
+            if let result = response.result.value {
+                
+                let json = JSON(result)
+                let result = json["isMansplaining"].boolValue
+                
+                if result {
+                    self.resultLabel.text = "YES. :("
+                } else {
+                    self.resultLabel.text = "YOU'RE GOOD. :)"
+                }
+                
+            } else {
+                self.resultLabel.text = "ERROR.  :/"
+            }
+        }
+    }
+        
+    
+    
 
+    
 
 }
 
